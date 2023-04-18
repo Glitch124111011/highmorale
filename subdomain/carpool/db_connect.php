1667b6e30d1e76f5f2fe4829b70f04671ba8fa8d
Glitch124111011 <fuertera@students.nu-baliwag.edu.ph>
2023-04-17 18:16:23 -0700

    commit 5

--- subdomain/carpool/db_connect.php
+++ subdomain/carpool/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$db_connection = mysqli_connect('ns1.dns-parking.com', 'u235219407_ramon','Ramon@15','u235219407_ramon');
+$db_connection = mysqli_connect('localhost', 'u235219407_ramon','Ramon@15','u235219407_ramon');
  if(mysqli_connect_errno()){
     echo "Failed to connect to MYSQL:" .mysqli_connect_error();
     exit();
