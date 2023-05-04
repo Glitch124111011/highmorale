0e77a88f9ff5103928e7ca74659a64b5927e6be0
Ramon Fuerte <fuertera@students.nu-baliwag.edu.ph>
2023-05-03 20:10:55 -0700

    new

--- subdomain/carpool/update.php
+++ subdomain/carpool/update.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 
 $sql = "UPDATE tbend_users SET verified = 'Y' WHERE user_id = '$id'";
 $result = mysqli_query($db_connection, $sql);    
-header("Location:table_registered.php");
+header("Location:login.html");
 exit();
 
 mysqli_close($db_connection);
