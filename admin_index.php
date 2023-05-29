f8b7c3688f321cbad21e92974aeef9a7929387cc
Ramon Fuerte <fuertera@students.nu-baliwag.edu.ph>
2023-05-29 08:34:37 -0700

    commit

--- admin_index.php
+++ admin_index.php
@@ -5,9 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Elegant Dashboard | Dashboard</title>
+  <title>HIgh Morale Dashboard</title>
   <!-- Favicon -->
-  <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
+
   <!-- Custom styles -->
   <link rel="stylesheet" href="css_admin/style.min.css">
   <link rel="stylesheet" href="css_admin/style.min.css">
