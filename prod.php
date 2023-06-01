49cf3f395e5a6ae00f99aac0bc42695890a8714b
Glitch124111011 <130850371+Glitch124111011@users.noreply.github.com>
2023-06-01 09:41:28 -0700

    Add files via upload

--- prod.php
+++ prod.php
@@ -59,7 +59,7 @@
         
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="index.php #message-section"> Contact</a>
+                  <a class="nav-link" href="index.php#message-section"> Contact</a>
                 </li>
                 
               </ul>
